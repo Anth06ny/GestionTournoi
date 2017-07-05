82b3f1b68363854a2e919995fd97ec0662158e91
Nicolas <nini_97137@hotmail.fr>
2017-07-05 14:16:03 +0200

    tournament detail activity layout

---
+++ app/src/main/java/com/anthony/gestiontournoi/model/wsbeans/WSUtilsMobile.java
@@ -0,0 +1,38 @@
+package com.anthony.gestiontournoi.model.wsbeans;
+
+import com.anthony.gestiontournoi.model.beans.TeamBean;
+import com.anthony.gestiontournoi.model.beans.TournamentBean;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Nicolas Th on 05/07/2017.
+ */
+
+public class WSUtilsMobile {
+
+    // MET A JOUR LES TABLES DE LA DATABASE MOBILE DEPUIS LE DB DISTANTE
+    public static void updateBean(){
+
+    }
+
+    // MET A JOUR LA TABLE TOURNAMENT DE LA DATABASE MOBILE DEPUIS LE DB DISTANTE
+    public static void updateTournament(){
+
+    }
+
+    // MET A JOUR LA TABLE TEAM DE LA DATABASE MOBILE DEPUIS LE DB DISTANTE
+    public static void updateTeam(){
+
+    }
+
+    // RETOURNE TOUS LES TOURNAMENTS DE LA DATABASE MOBILE
+    public static ArrayList<TournamentBean> getAllTournament(){
+        return null;
+    }
+
+    // RETOURNE TOUTES LES TEAMS DE LA DATABASE MOBILE
+    public static ArrayList<TeamBean> getAllTeam(){
+        return null;
+    }
+}
---
+++ app/src/main/java/com/anthony/gestiontournoi/model/wsbeans/WSUtilsServer.java
@@ -0,0 +1,11 @@
+package com.anthony.gestiontournoi.model.wsbeans;
+
+/**
+ * Created by Nicolas Th on 05/07/2017.
+ */
+
+public class WSUtilsServer {
+    public static void updateTournament(long timestamp){
+
+    }
+}
